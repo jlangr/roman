197d8edd56e43f8c9f193938a1dc1367639a579c
jlangr <jeff@langrsoft.com>
2012-11-20 11:03:47 -0700

    2

--- RomanConverterTest.cpp
+++ RomanConverterTest.cpp
@@ -3,8 +3,18 @@
 using namespace ::testing;
 using namespace std;
 
+// START:convert
+string convert(unsigned int arabic) 
+{
+   if (arabic == 2)
+      return "II";
+   return "I";
+}
+// END:convert
+
 // START:test
 TEST(RomanConverter, CanConvertPositiveDigits) {
    ASSERT_THAT(convert(1), Eq("I"));
+   ASSERT_THAT(convert(2), Eq("II"));
 }
 // END:test
