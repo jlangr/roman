02fe92c157ed76acb6de4ecd4c09172a2e0d168a
jlangr <jeff@langrsoft.com>
2012-11-20 11:51:46 -0700

    fix

--- RomanConverterTest.cpp
+++ RomanConverterTest.cpp
@@ -3,6 +3,11 @@
 using namespace ::testing;
 using namespace std;
 
+string convert(unsigned int arabic)
+{
+   return "I";
+}
+
 // START:test
 TEST(RomanConverter, CanConvertPositiveDigits) {
    EXPECT_THAT(convert(1), Eq("I"));
