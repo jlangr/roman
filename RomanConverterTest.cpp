b3cb4b13bde54768020d2c1a5e01815dd6182818
jlangr <jeff@langrsoft.com>
2012-11-20 13:21:15 -0700

    XX

--- RomanConverterTest.cpp
+++ RomanConverterTest.cpp
@@ -7,7 +7,7 @@ using namespace std;
 string convert(unsigned int arabic) 
 {
    string roman("");
-   if (arabic >= 10)
+   while (arabic >= 10)
    {
       roman += "X";
       arabic -= 10;
@@ -24,12 +24,11 @@ TEST(RomanConverter, CanConvertPositiveDigits) {
    EXPECT_THAT(convert(2), Eq("II"));
    EXPECT_THAT(convert(3), Eq("III"));
    EXPECT_THAT(convert(10), Eq("X"));
-// START:assert11
    EXPECT_THAT(convert(11), Eq("XI"));
-// END:assert11
-// START:assert12and13
    EXPECT_THAT(convert(12), Eq("XII"));
    EXPECT_THAT(convert(13), Eq("XIII"));
-// END:assert12and13
+// START:assert20
+   EXPECT_THAT(convert(20), Eq("XX"));
+// START:assert20
 }
 // END:test
