b88582129709f670c180faf97867349240f587ef
jlangr <jeff@langrsoft.com>
2013-05-08 17:09:19 -0600

    fix

--- RomanConverterTest.cpp
+++ RomanConverterTest.cpp
@@ -6,7 +6,7 @@ using namespace std;
 // START:convert
 string convert(unsigned int arabic) 
 {
-   string roman("");
+   string roman{""};
    while (arabic-- > 0)
       roman += "I";
    return roman;
